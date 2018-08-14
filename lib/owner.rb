2e1299037a6ec2939bd6e1bd019a899036a11758
Eli Cho <eli.cho@gmail.com>
2018-08-14 00:53:27 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,22 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@all = []
+  
+  
+  def initialize(species)
+    @species = species
+    @@all << self
+    @pets = {:cats => [], :dogs => [], :fishes => []}
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
+  
+  def pets
+    
+  end
+
 end
